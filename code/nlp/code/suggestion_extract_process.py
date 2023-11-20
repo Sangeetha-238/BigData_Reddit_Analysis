6eb5ab12c3c1dc9bba4284afb374138a8bb5c7ae
Ramdayal <rr1220@georgetown.edu>
2023-11-20 21:28:40 +0000

    Converting to Spark NLP Job

--- code/nlp/code/suggestion_extract_process.py
+++ code/nlp/code/suggestion_extract_process.py
@@ -7,6 +7,8 @@ print(f"Python version: {sys.version}")
 print(f"Python executable: {sys.executable}")
 print(sys.path)
 
+subprocess.check_call([sys.executable, "-m", "pip", "install", "spark-nlp"])
+
 import os
 import logging
 import argparse
@@ -23,20 +25,24 @@ from pyspark.sql.types import (
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col
 from pyspark.sql.functions import udf
+import pyspark.sql.functions as F
 from pyspark.sql.types import ArrayType
 import re
 from pyspark.sql.functions import explode, count
+import sagemaker
+from pyspark.sql.functions import lower, regexp_replace, col, concat_ws
+from pyspark.ml.feature import Tokenizer, StopWordsRemover
+from sparknlp.annotator import *
+from sparknlp.base import *
+import sparknlp
+from sparknlp.pretrained import PretrainedPipeline
+from sparknlp.base import Finisher, DocumentAssembler
 
-spacy_version = "3.7.2"  
-thinc_version = "8.2.1" 
-pydantic_version = "1.8.0" 
+from pyspark.sql.functions import desc
 
-# Install the packages using pip
-subprocess.check_call([sys.executable, "-m", "pip", "install", f"spacy=={spacy_version}"])
-subprocess.check_call([sys.executable, "-m", "pip", "install", f"thinc=={thinc_version}"])
-subprocess.check_call([sys.executable, "-m", "pip", "install", f"pydantic=={pydantic_version}"])
-subprocess.check_call([sys.executable, "-m", "spacy", "download", "en_core_web_sm"])
-import spacy
+import nltk
+nltk.download('stopwords')
+eng_stopwords = nltk.corpus.stopwords.words('english')
 
 logging.basicConfig(format='%(asctime)s,%(levelname)s,%(module)s,%(filename)s,%(lineno)d,%(message)s', level=logging.DEBUG)
 logger = logging.getLogger()
@@ -50,7 +56,18 @@ def main():
     parser.add_argument("--col_name_for_filtering", type=str, help="Name of the column to filter")
     args = parser.parse_args()
 
-    spark = SparkSession.builder.appName("PySparkApp").getOrCreate()
+    spark = SparkSession.builder \
+    .appName("Spark NLP")\
+    .master("local[*]")\
+    .config("spark.driver.memory","16G")\
+    .config("spark.driver.maxResultSize", "0") \
+    .config("spark.kryoserializer.buffer.max", "2000M")\
+    .config("spark.jars.packages", "com.johnsnowlabs.nlp:spark-nlp_2.12:5.1.3,org.apache.hadoop:hadoop-aws:3.2.2")\
+    .config(
+            "fs.s3a.aws.credentials.provider",
+            "com.amazonaws.auth.ContainerCredentialsProvider",
+    )\
+    .getOrCreate()
     logger.info(f"spark version = {spark.version}")
     
     # This is needed to save RDDs which is the only way to write nested Dataframes into CSV format
@@ -69,41 +86,63 @@ def main():
     # DATA CLEANING
     comments_filtered = df_filtered.filter((df.body != '[deleted]') & (df.author != '[deleted]'))
 
-    # Load spaCy model
-    nlp = spacy.load("en_core_web_sm")
+    # Define the pipeline stages
+    document_assembler = DocumentAssembler() \
+        .setInputCol("body") \
+        .setOutputCol("document")
 
-    # Define schema for the UDF output
-    movie_schema = StructType([
-        StructField("movie_positions", ArrayType(ArrayType(StringType()))),
-        StructField("movie_names", ArrayType(StringType()))
+    sentence_detector = SentenceDetector() \
+        .setInputCols(["document"]) \
+        .setOutputCol("sentence")
+
+    tokenizer = Tokenizer() \
+        .setInputCols(["sentence"]) \
+        .setOutputCol("token")
+
+    # Use a pretrained embeddings model, for example, BERT
+    embeddings = BertEmbeddings.pretrained("bert_base_cased", "en") \
+        .setInputCols(["sentence", "token"]) \
+        .setOutputCol("embeddings")
+
+    ner_model = NerDLModel.pretrained("ner_dl_bert", "en") \
+        .setInputCols(["sentence", "token", "embeddings"]) \
+        .setOutputCol("ner")
+
+    ner_converter = NerConverter() \
+        .setInputCols(["sentence", "token", "ner"]) \
+        .setOutputCol("ner_chunk")
+
+    # Build the pipeline
+    nlp_pipeline = Pipeline(stages=[
+        document_assembler,
+        sentence_detector,
+        tokenizer,
+        embeddings,
+        ner_model,
+        ner_converter
     ])
 
-    # UDF to extract movie names
-    @udf(movie_schema)
-    def extract_movie_names_udf(text):
-        doc = nlp(text)
-        movie_positions = []
-        movie_names = []
+    # Apply the pipeline to your DataFrame
+    model = nlp_pipeline.fit(comments_filtered_movies)
+    result = model.transform(comments_filtered_movies)
 
-        for ent in doc.ents:
-            if ent.label_ == "ORG" or ent.label_ == "PERSON":
-                movie_positions.append([ent.start_char, ent.end_char])
-                movie_names.append(ent.text)
+    # Define a UDF to filter and extract movie names
+    def extract_movies(chunks):
+        movie_names = [chunk.result for chunk in chunks if chunk.metadata['entity'] in ['PERSON', 'ORG']]
+        return movie_names
 
-        return (movie_positions, movie_names)
+    extract_movie_names_udf = udf(extract_movies, ArrayType(StringType()))
 
-    # UDF to remove movie names
+    # Apply the UDF to the DataFrame
+    movies_df = result.withColumn("movie_names", extract_movie_names_udf(F.col("ner_chunk")))
+
+
     @udf(StringType())
-    def remove_movie_names_udf(text, movie_positions):
-        # Reverse sort positions to avoid shifting positions
-        if movie_positions:
-            movie_positions = sorted([(int(start), int(end)) for start, end in movie_positions], key=lambda x: x[0], reverse=True)
-
-            for start, end in movie_positions:
-                text = text[:start] + ' ' + text[end:]
-
+    def remove_movie_names_udf(text, movie_names):
+        if movie_names:
+            for name in movie_names:
+                text = text.replace(name, ' ')
             return ' '.join(text.split())
-
         else:
             return text
 
@@ -113,36 +152,40 @@ def main():
         movie_name_pattern = r'(?:\"([^\"]+)\"|([A-Z][a-z]*(?:\s+(?:[a-z]+\s+)*[A-Z][a-z]*)*)(?: \(\d{4}\))?)'
 
         movie_matches = re.findall(movie_name_pattern, text)
-        movies = [m for match in matches for m in match if m]
+        movies = [match[0] or match[1] or match[2] for match in movie_matches]
         return movie_names + movies
     
-    def remove_stop_word_from_movie_names(suggestion_list):
-        
-        if suggestion_list and len(suggestion_list[0].split()) == 1 and suggestion_list[0].lower() in stop_words:
-            return suggestion_list[1:]
-        
-        return suggestion_list
+    def remove_stop_word_from_movie_names(movies):
+        if movies and len(movies[0].split()) == 1 and movies[0].lower() in eng_stopwords:
+            return movies[1:]
+        return movies
 
-    # Applying the UDFs to the DataFrame
-    df_with_suggestions = comments_filtered.withColumn("movie_data", extract_movie_names_udf(comments_filtered["body"]))
-    df_removed_suggestions_names = df_with_suggestions.withColumn("body_no_movies", remove_movie_names_udf(comments["body"], df_with_movie_data["movie_data.movie_positions"]))
-    df_final = df_removed_suggestions_names.withColumn("suggestion_list", extract_movie_names_regex_udf(df_removed_movie_names["body_no_movies"], df_removed_movie_names["movie_data.movie_names"]))
-    
-    df_final = df_final.select("subreddit", "author", "body", "parent_id", "id", "created_utc", "score", "controversiality", "suggestion_list")
-    
-    stop_words = set(["a", "an", "the", "and", "or", "but", "is", "are", "was", "were", "be", "been", "being", "there", "he", "she"])  # stop words
-    
-    df_final = df_final.withColumn("suggestion_lists", remove_stop_word_udf(df_final["suggestion_list"]))
+    # Remove movie names from the 'body' text
+    df_removed_movie_names = movies_df.withColumn("body_no_movies", remove_movie_names_udf(movies_df["body"], movies_df["movie_names"]))
+
+    # If you still want to use the regex method to supplement the NER extraction
+    df_final = df_removed_movie_names.withColumn("additional_movie_names", extract_movie_names_regex_udf(df_removed_movie_names["body_no_movies"], df_removed_movie_names["movie_names"]))
+
+    df_final = df_final.select("subreddit", "author", "body", "parent_id", "id", "created_utc", "score", "controversiality", "additional_movie_names")
     
     remove_stop_word_udf = udf(remove_stop_word_from_movie_names, ArrayType(StringType()))
 
+    df_final = df_final.withColumn("movie_names_final", remove_stop_word_udf(df_final["additional_movie_names"]))
+    
     # Flatten the movie_names column
-    df_flattened = df_final_sample.withColumn("suggestions", explode(col("suggestion_lists")))
+    df_flattened = df_final.withColumn("movie_name", explode(col("movie_names_final")))
 
     # Group by movie_name and count the occurrences
-    df_suggestion_frequency = df_flattened.groupBy("suggestions").agg(count("*").alias("frequency"))
+    df_frequency = df_flattened.groupBy("movie_name").agg(count("*").alias("frequency"))
+    
+    # Sort the DataFrame by frequency in descending order and take the top 1000
+    df_top_1000_movies = df_frequency.orderBy(desc("frequency")).limit(1000)
+    
+    bucket = "project-group34"
+    output_prefix_data_comments = "project/comments/"
+    s3_path = f"s3a://{bucket}/{output_prefix_data_comments}"
 
-    df_suggestion_frequency.write.mode("overwrite").parquet("{s3_path}/nlp/")
+    df_top_1000_movies.write.mode("overwrite").parquet(f"{s3_path}/movie_suggestions/")
     
     logger.info(f"all done...")
     
