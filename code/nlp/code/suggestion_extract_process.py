65c654863c8bafd25376187fdf4d7b02ab41cfa0
Ramdayal <rr1220@georgetown.edu>
2023-11-26 21:18:49 +0000

    Anime NLP Done

--- code/nlp/code/suggestion_extract_process.py
+++ code/nlp/code/suggestion_extract_process.py
@@ -1,14 +1,4 @@
 
-import subprocess
-import sys
-
-import sys
-print(f"Python version: {sys.version}")
-print(f"Python executable: {sys.executable}")
-print(sys.path)
-
-subprocess.check_call([sys.executable, "-m", "pip", "install", "spark-nlp"])
-
 import os
 import logging
 import argparse
@@ -37,6 +27,8 @@ from sparknlp.base import *
 import sparknlp
 from sparknlp.pretrained import PretrainedPipeline
 from sparknlp.base import Finisher, DocumentAssembler
+from sparknlp.annotator import SentenceDetector, Tokenizer, WordEmbeddingsModel, NerDLModel, NerConverter
+from pyspark.ml import Pipeline
 
 from pyspark.sql.functions import desc
 
@@ -79,10 +71,10 @@ def main():
     vals = [args.col_name_for_filtering]
     df_filtered = df.where(col("subreddit").isin(vals))
     logger.info(f"finished reading files...")
+    logger.info(f"Number of rows in data: {df_filtered.count()}")
     
     # DATA CLEANING
-    comments_filtered = df_filtered.filter((df.body != '[deleted]') & (df.author != '[deleted]'))
-    comments_filtered_movies = comments_filtered.where(col("subreddit").isin("MovieSuggestions"))
+    df_filtered = df_filtered.filter((df.body != '[deleted]') & (df.author != '[deleted]'))
 
     # Define the pipeline stages
     document_assembler = DocumentAssembler() \
@@ -97,12 +89,13 @@ def main():
         .setInputCols(["sentence"]) \
         .setOutputCol("token")
 
-    # Use a pretrained embeddings model, for example, BERT
-    embeddings = BertEmbeddings.pretrained("bert_base_cased", "en") \
+    # Use GloVe embeddings
+    embeddings = WordEmbeddingsModel.pretrained("glove_100d", "en") \
         .setInputCols(["sentence", "token"]) \
         .setOutputCol("embeddings")
 
-    ner_model = NerDLModel.pretrained("ner_dl_bert", "en") \
+    # Use a lighter NER model
+    ner_model = NerDLModel.pretrained("ner_dl", "en") \
         .setInputCols(["sentence", "token", "embeddings"]) \
         .setOutputCol("ner")
 
@@ -121,83 +114,24 @@ def main():
     ])
 
     # Apply the pipeline to your DataFrame
-    model = nlp_pipeline.fit(comments_filtered_movies)
-    result = model.transform(comments_filtered_movies)
-    
+    model = nlp_pipeline.fit(df_filtered)
+    result = model.transform(df_filtered)
+
     print("NLP Pipeline Ran Succesfully!")
+                
+    result = result.select("subreddit", "author", "body", "parent_id", "id", "created_utc", "score", "controversiality", "ner_chunk")
 
-    # Define a UDF to filter and extract movie names
-    def extract_movies(chunks):
-        movie_names = [chunk.result for chunk in chunks if chunk.metadata['entity'] in ['PERSON', 'ORG']]
-        return movie_names
+    # Define a UDF to filter and extract anime names
+    def extract_anime(chunks):
+        anime_names = [chunk.result for chunk in chunks if chunk.metadata['entity'] in ['PERSON', 'ORG']]
+        return anime_names
 
-    extract_movie_names_udf = udf(extract_movies, ArrayType(StringType()))
+    extract_anime_names_udf = udf(extract_anime, ArrayType(StringType()))
 
     # Apply the UDF to the DataFrame
-    movies_df = result.withColumn("movie_names", extract_movie_names_udf(F.col("ner_chunk")))
-
-
-    @udf(StringType())
-    def remove_movie_names_udf(text, movie_names):
-        if movie_names:
-            for name in movie_names:
-                text = text.replace(name, ' ')
-            return ' '.join(text.split())
-        else:
-            return text
-
-    # UDF to extract movie names using regex
-    @udf(ArrayType(StringType()))
-    def extract_movie_names_regex_udf(text, movie_names):
-        movie_name_pattern = r'(?:\"([^\"]+)\"|([A-Z][a-z]*(?:\s+(?:[a-z]+\s+)*[A-Z][a-z]*)*)(?: \(\d{4}\))?)'
-
-        movie_matches = re.findall(movie_name_pattern, text)
-        movies = [match[0] or match[1] or match[2] for match in movie_matches]
-        return movie_names + movies
+    anime_df = result.withColumn("movie_names", extract_anime_names_udf(F.col("ner_chunk")))
     
-    def remove_stop_word_from_movie_names(movies):
-        if movies and len(movies[0].split()) == 1 and movies[0].lower() in eng_stopwords:
-            return movies[1:]
-        return movies
-
-    # Remove movie names from the 'body' text
-    df_removed_movie_names = movies_df.withColumn("body_no_movies", remove_movie_names_udf(movies_df["body"], movies_df["movie_names"]))
-
-    # The regex method to supplement the NER extraction
-    df_final = df_removed_movie_names.withColumn("additional_movie_names", extract_movie_names_regex_udf(df_removed_movie_names["body_no_movies"], df_removed_movie_names["movie_names"]))
-
-    df_final = df_final.select("subreddit", "author", "body", "parent_id", "id", "created_utc", "score", "controversiality", "additional_movie_names")
-    
-    print("Movie Names Extracted")
-    
-    remove_stop_word_udf = udf(remove_stop_word_from_movie_names, ArrayType(StringType()))
-
-    df_final = df_final.withColumn("movie_names_final", remove_stop_word_udf(df_final["additional_movie_names"]))
-    
-    # Flatten the movie_names column
-    df_flattened = df_final.withColumn("movie_name", explode(col("movie_names_final")))
-
-    # Group by movie_name and count the occurrences
-    df_frequency = df_flattened.groupBy("movie_name").agg(count("*").alias("frequency"))
-    
-    print("Aggregation Done!")
-    
-    # Sort the DataFrame by frequency in descending order and take the top 1000
-    df_top_1000_movies = df_frequency.orderBy(desc("frequency")).limit(1000)
-    
-    # df_top_1000_movies_pd = df_top_1000_movies.toPandas()
-    
-    bucket = "project-group34"
-    output_prefix_data_comments = "project/comments/"
-    s3_path = f"s3a://{bucket}/{output_prefix_data_comments}" + args.col_name_for_filtering + "/"
-    
-    print(f"Writing to {s3_path}")
-    df_top_1000_movies.write.parquet(s3_path)
-    # Save the DataFrame in CSV format to S3
-    #df_top_1000_movies.write.option("header", "true").mode("overwrite").csv(s3_path)
-    print(f"Finished writing to {s3_path}")
-
-    # df_top_1000_movies.to_csv(f"{s3_path}/movie_suggestions/{csv_name}")
+    anime_df.write.parquet("s3a://project-group34/project/suggestions/all_anime/ner/", mode="overwrite")
     
     logger.info(f"all done...")
     
